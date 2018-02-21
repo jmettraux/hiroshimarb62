39aaf7adcd56dda7463c68d915cb9828ac255636
John Mettraux <jmettraux@gmail.com>
2018-02-21 19:19:11 +0900

    Implement crypt.rb

---
+++ crypt.rb
@@ -0,0 +1,28 @@
+
+require 'ffi'
+
+module Fubar
+  extend FFI::Library
+
+  # man 3 crypt
+  #
+  # char *crypt(const char *key, const char *salt);
+
+  ffi_lib 'c'
+
+  attach_function :crypt, [ :string, :string ], :string
+end
+
+
+if $0 == __FILE__
+
+  string, salt = ARGV
+
+  if [ nil, '-h', '--help' ].include?(string)
+    puts "ruby #{$0} string [salt]"
+    puts "  --> prints the result of crypt(string, salt || 'NaCl')"
+  else
+    puts Fubar.crypt(string, salt || 'NaCl')
+  end
+end
+
