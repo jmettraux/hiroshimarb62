611a47cf28fc7fd82ed02742abf0d590b1268fde
John Mettraux <jmettraux@gmail.com>
2018-02-22 16:42:33 +0900

    Refine webevent.rb

--- webevent.rb
+++ webevent.rb
@@ -2,32 +2,85 @@
 require 'ffi'
 
 
-module WebEvent
-  extend FFI::Library
+class WebServer
 
-  ffi_lib '/usr/local/lib/libevent.dylib'
+  module Ev
+    extend FFI::Library
 
-  attach_function :event_init, [], :pointer
-  attach_function :event_dispatch, [], :int
-  attach_function :evbuffer_new, [], :pointer
-  attach_function :evbuffer_add, [ :pointer, :string, :int], :pointer
-  attach_function :evbuffer_free, [ :pointer ], :void
-  callback :http_callback, [ :pointer, :pointer ], :void
-  attach_function :evhttp_start, [ :string, :int ], :pointer
-  attach_function :evhttp_set_gencb, [ :pointer, :http_callback, :int ], :int
-  attach_function :evhttp_send_reply, [ :pointer, :int, :string, :pointer ], :void
+    ffi_lib '/usr/local/lib/libevent.dylib'
+
+    attach_function :event_init, [], :pointer
+    attach_function :event_dispatch, [], :int
+    attach_function :evbuffer_new, [], :pointer
+    attach_function :evbuffer_add, [ :pointer, :string, :int], :pointer
+    attach_function :evbuffer_free, [ :pointer ], :void
+    callback :http_callback, [ :pointer, :pointer ], :void
+    attach_function :evhttp_start, [ :string, :int ], :pointer
+    attach_function :evhttp_set_gencb, [ :pointer, :http_callback, :int ], :int
+    attach_function :evhttp_send_reply, [ :pointer, :int, :string, :pointer ], :void
+    attach_function :evhttp_request_get_uri, [ :pointer ], :string
+  end
+
+  def initialize
+
+    @paths = []
+
+    @callback =
+      Proc.new do |request, args|
+        begin
+
+          t = Time.now
+
+          buffer = Ev.evbuffer_new
+          path = Ev.evhttp_request_get_uri(request)
+
+          _, block = @paths.find { |pa, bl| pa == path }
+
+          icode, scode, text =
+            if block
+              [ 200, 'OK', block.call ]
+            else
+              [ 404, 'Not Found', '.' ]
+            end
+
+          Ev.evbuffer_add(buffer, text, text.length)
+          Ev.evhttp_send_reply(request, icode, scode, buffer)
+
+          Ev.evbuffer_free(buffer)
+
+          puts "GET #{path} #{icode} took #{(Time.now - t) * 1000.0}ms"
+
+        rescue => err
+
+          p err
+        end
+      end
+  end
+
+  def start
+
+    Ev.event_init
+
+    httpd = Ev.evhttp_start('0.0.0.0', 8000)
+    Ev.evhttp_set_gencb(httpd, @callback, 0)
+
+    puts "PID #{$$} listening on port 8000..."
+
+    Ev.event_dispatch
+  end
+
+  def serve(path, &block)
+
+    @paths << [ path, block ]
+  end
 end
 
-HttpCallback = Proc.new do |request, args|
-  buf = WebEvent.evbuffer_new
-  WebEvent.evbuffer_add(buf, "hello world !\n", 4)
-  WebEvent.evhttp_send_reply(request, 200, 'OK', buf)
-  WebEvent.evbuffer_free(buf)
+
+server = WebServer.new
+
+server.serve('/') do
+  "Hello libevent world!\n"
 end
 
-WebEvent.event_init
-httpd = WebEvent.evhttp_start('0.0.0.0', 8000)
-WebEvent.evhttp_set_gencb(httpd, HttpCallback, 0)
-puts "listening on port 8000..."
-WebEvent.event_dispatch
+server.start
 
